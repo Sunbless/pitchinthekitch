336b1c152ff85bc04ff4b3c8c59e87f3aefc119e
Sunbless <micic.tanja@gmail.com>
2018-02-27 09:24:45 +0100

    Last commit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {latestNews} from '../actions';
+import {bindActionCreators} from 'redux';
+
+//component
+import LatestNews from '../components/Home/Latest'
+
 
 class Home extends  Component{
+
+    componentDidMount(){
+        this.props.latestNews()
+    }
+
     render(){
         return(
             <div>
-                home
+                <LatestNews latest={this.props.articles.latest}/>
             </div>
         )
     }
 }
 
-export default Home;
+function mapStateToProps(state){
+    return{
+        articles:state.articles
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({latestNews}, dispatch)
+}
+    
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home )
